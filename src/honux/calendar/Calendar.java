82c8517a049f7025f561cdd8618bfdd76d381dd7
トマト <@DESKTOP-E0F80M6>
2018-04-13 11:50:43 +0900

    part 3-8 outputting as many as the number of month that I input

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -5,7 +5,30 @@ import java.util.Scanner;
 public class Calendar {
 
 	public static void main(String[] args) {
-// 해달 월의 일수를 구해줍니다.
+		// 해달 월의 일수를 구해줍니다.
+		int[] daysOfEachMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		int number = 0;
+
+		// 繰り返しの回数
+		System.out.println("繰り返しの回数を入力してください");
+		Scanner scanner = new Scanner(System.in);
+		int numberOfMonth = scanner.nextInt();
+		int[] numberArray = new int[numberOfMonth];
+		// 月を入力してください。
+
+		System.out.println("月を入力してください。");
+		for (int i = 0; i < numberOfMonth; i++) {
+			number = scanner.nextInt();
+			numberArray[i] = number;
+
+		}
+		for (int i = 0; i < numberOfMonth; i++) {
+			number = numberArray[i];
+			System.out.printf("%d월은 %d일까지 있습니다. \n", number, daysOfEachMonth[number - 1]);
+		}
+
+		scanner.close();
+
 		System.out.println("  일   월   화   수   목   금   토");
 		System.out.println("----------------------");
 		System.out.println("  1  2  3  4  5  6  7");
@@ -13,21 +36,5 @@ public class Calendar {
 		System.out.println(" 15 16 17 18 19 20 21");
 		System.out.println(" 22 23 24 25 26 27 28");
 
-		Scanner scanner = new Scanner(System.in);
-		String number = scanner.nextLine();
-		int k = Integer.parseInt(number);
-		int[] j = { 28, 30, 31 };
-		if (k == 1 || k == 3 || k == 5 || k == 7 || k == 8 || k == 10 || k == 12) {
-			System.out.printf("%d월은 %d일까지 있습니다.", k, j[2]);
-		}
-		;
-		if (k == 4 || k == 6 || k == 9 || k == 11) {
-			System.out.printf("%d월은 %d일까지 있습니다.", k, j[1]);
-		}
-		;
-		if (k == 2) {
-			System.out.printf("%d월은 %d일까지 있습니다.", k, j[0]);
-		}
-		scanner.close();
 	}
 }
