87fcc77e130a74d3c073bc41a4d8fde346e66615
トマト <@DESKTOP-E0F80M6>
2018-04-13 13:53:14 +0900

    on making the Calendar

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -4,38 +4,30 @@ import java.util.Scanner;
 
 public class Calendar {
 
-	public static void main(String[] args) {
-		// 해달 월의 일수를 구해줍니다.
-		int[] daysOfEachMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-		int number = 0;
-
-		// 繰り返しの回数
-		System.out.println("繰り返しの回数を入力してください");
-		Scanner scanner = new Scanner(System.in);
-		int numberOfMonth = scanner.nextInt();
-		int[] numberArray = new int[numberOfMonth];
+public static void CalendarShowing(int year, int month) {
+	int[] daysOfEachMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+	Scanner scanner = 
 		// 月を入力してください。
 
-		for (int i = 0; i < numberOfMonth; i++) {
+		while(true) {
 			System.out.println("月を入力してください。");
 			System.out.print("cal> ");
-			number = scanner.nextInt();
+			
+			int number = scanner.nextInt();
 			if (number != -1) {
-				numberArray[i] = number;
+
+		
 				System.out.printf("%d월은 %d일까지 있습니다. \n", number, daysOfEachMonth[number - 1]);
+				System.out.printf("        <%4d년%3d월 달력>        \n", number );
+				System.out.println("  일   월   화   수   목   금   토");
+				System.out.println("----------------------");
+				for (int i = 1; i <= daysOfEachMonth[number-1]; i++) {
+					System.out.printf("%3d/n",i);
+				}
+				
 			} else {
-				System.out.println("Have a great day!!");
-			}
+
 		}
-
-		scanner.close();
-
-		System.out.println("  일   월   화   수   목   금   토");
-		System.out.println("----------------------");
-		System.out.println("  1  2  3  4  5  6  7");
-		System.out.println("  8  9 10 11 12 13 14");
-		System.out.println(" 15 16 17 18 19 20 21");
-		System.out.println(" 22 23 24 25 26 27 28");
-
 	}
+}
 }
---
+++ src/honux/calendar/Prompt.java
@@ -0,0 +1,24 @@
+package honux.calendar;
+
+import java.util.Scanner;
+
+public class Prompt {
+
+	public static void main(String[] args) {
+		// 해달 월의 일수를 구해줍니다.
+
+		int number = 0;
+
+		// 繰り返しの回数
+		System.out.println("繰り返しの回数を入力してください");
+		Scanner scanner = new Scanner(System.in);
+}				
+	System.out.println("Have a great day!!");
+	scanner.close();
+	break;
+
+	Calendar();
+}
+
+
+
