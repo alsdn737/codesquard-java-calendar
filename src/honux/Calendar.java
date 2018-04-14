cce9a769030fbcf8357828336070d7d72a0467e2
alsdn737 <alsdn737@192.168.11.4>
2018-04-15 07:47:14 +0900

    the day of the week has been adjusted

--- src/honux/Calendar.java
+++ src/honux/Calendar.java
@@ -2,33 +2,168 @@ package honux;
 
 public class Calendar {
 
-	public static void CalendarShowing(int year, int month) {
-		int[] daysOfEachMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 29 };
-
+	public static void CalendarShowing(int year, int month, String day) {
+		int[] datesOfEachMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31, 29 };
 		if (month != -1) {
 
-			System.out.printf("%d월은 %d일까지 있습니다. \n", month, daysOfEachMonth[month - 1]);
+			System.out.printf("%d월은 %d일까지 있습니다. \n", month, datesOfEachMonth[month - 1]);
 			System.out.printf("   <%4d년%3d월 달력>   \n", year, month);
 			System.out.println("  일   월   화   수   목   금   토");
 			System.out.println("----------------------");
 
 			if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
-				for (int i = 1; i <= daysOfEachMonth[12]; i++) {
-					System.out.printf("%3d", i);
-					if (i % 7 == 0) {
-						System.out.println();
+				switch (day) {
+				case "SU":
+					for (int i = 1; i <= datesOfEachMonth[12]; i++) {
+						System.out.printf("%3d", i);
+						// 일요일부터 시작하는 달은
+						if (i % 7 == 0) {
+							System.out.println();
+						}
+					}
+					break;
+				case "MO":
+					System.out.print("   ");
+					for (int i = 1; i <= datesOfEachMonth[12]; i++) {
+						System.out.printf("%3d", i);
+						// 월요일부터 시작하는 달은
+						if (i % 7 == 6) {
+							System.out.println();
+						}
+					}
+					break;
+				case "TU":
+					System.out.printf("      ");
+					for (int i = 1; i <= datesOfEachMonth[12]; i++) {
+						System.out.printf("%3d", i);
+						// 월요일부터 시작하는 달은
+						if (i % 7 == 5) {
+							System.out.println();
+						}
 					}
 
+					break;
+				case "WE":
+					System.out.printf("         ");
+					for (int i = 1; i <= datesOfEachMonth[12]; i++) {
+						System.out.printf("%3d", i);
+						// 월요일부터 시작하는 달은
+						if (i % 7 == 4) {
+							System.out.println();
+						}
+					}
+
+					break;
+				case "TH":
+					System.out.printf("            ");
+					for (int i = 1; i <= datesOfEachMonth[12]; i++) {
+						System.out.printf("%3d", i);
+						// 월요일부터 시작하는 달은
+						if (i % 7 == 3) {
+							System.out.println();
+						}
+					}
+
+					break;
+				case "FR":
+					System.out.printf("               ");
+					for (int i = 1; i <= datesOfEachMonth[12]; i++) {
+						System.out.printf("%3d", i);
+						// 월요일부터 시작하는 달은
+						if (i % 7 == 2) {
+							System.out.println();
+						}
+					}
+
+					break;
+				case "SA":
+					System.out.printf("                  ");
+					for (int i = 1; i <= datesOfEachMonth[12]; i++) {
+						System.out.printf("%3d", i);
+						// 월요일부터 시작하는 달은
+						if (i % 7 == 1) {
+							System.out.println();
+						}
+					}
+					break;
 				}
 				System.out.println();
 			} else {
 
-				for (int i = 1; i <= daysOfEachMonth[month - 1]; i++) {
-					System.out.printf("%3d", i);
-					if (i % 7 == 0) {
-						System.out.println();
+				switch (day) {
+				case "SU":
+					for (int i = 1; i <= datesOfEachMonth[month - 1]; i++) {
+						System.out.printf("%3d", i);
+						// 일요일부터 시작하는 달은
+						if (i % 7 == 0) {
+							System.out.println();
+						}
+					}
+					break;
+				case "MO":
+					System.out.print("   ");
+					for (int i = 1; i <= datesOfEachMonth[month - 1]; i++) {
+						System.out.printf("%3d", i);
+						// 월요일부터 시작하는 달은
+						if (i % 7 == 6) {
+							System.out.println();
+						}
+					}
+					break;
+				case "TU":
+					System.out.printf("      ");
+					for (int i = 1; i <= datesOfEachMonth[month - 1]; i++) {
+						System.out.printf("%3d", i);
+						// 월요일부터 시작하는 달은
+						if (i % 7 == 5) {
+							System.out.println();
+						}
 					}
 
+					break;
+				case "WE":
+					System.out.printf("         ");
+					for (int i = 1; i <= datesOfEachMonth[month - 1]; i++) {
+						System.out.printf("%3d", i);
+						// 월요일부터 시작하는 달은
+						if (i % 7 == 4) {
+							System.out.println();
+						}
+					}
+
+					break;
+				case "TH":
+					System.out.printf("            ");
+					for (int i = 1; i <= datesOfEachMonth[month - 1]; i++) {
+						System.out.printf("%3d", i);
+						// 월요일부터 시작하는 달은
+						if (i % 7 == 3) {
+							System.out.println();
+						}
+					}
+
+					break;
+				case "FR":
+					System.out.printf("               ");
+					for (int i = 1; i <= datesOfEachMonth[month - 1]; i++) {
+						System.out.printf("%3d", i);
+						// 월요일부터 시작하는 달은
+						if (i % 7 == 2) {
+							System.out.println();
+						}
+					}
+
+					break;
+				case "SA":
+					System.out.printf("                  ");
+					for (int i = 1; i <= datesOfEachMonth[month - 1]; i++) {
+						System.out.printf("%3d", i);
+						// 월요일부터 시작하는 달은
+						if (i % 7 == 1) {
+							System.out.println();
+						}
+					}
+					break;
 				}
 				System.out.println();
 			}
--- src/honux/Prompt.java
+++ src/honux/Prompt.java
@@ -7,32 +7,35 @@ public class Prompt {
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		// 해달 월의 일수를 구해줍니다.
-final String YEAR_PROMPT = "year> ";
-final String MONTH_PROMPT = "month> ";
+		final String YEAR_PROMPT = "year> ";
+		final String MONTH_PROMPT = "month> ";
+		final String DAY_PROMPT = "day> ";
 
 		int year = 0;
-		int month = 0;		
-	while(true){
-		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("년을 입력하세요.");
-		System.out.print(YEAR_PROMPT);
-			 year = scanner.nextInt();
-			
+		int month = 0;
+		String day = "";
+		while (true) {
+			Scanner scanner = new Scanner(System.in);
+
+			System.out.println("년을 입력하세요.");
+			System.out.print(YEAR_PROMPT);
+			year = scanner.nextInt();
+
 			System.out.println("월을 입력하세요.");
 			System.out.print(MONTH_PROMPT);
-			 month = scanner.nextInt();
-			 
-			 Calendar.CalendarShowing(year, month);
-			 
-			 if(month == -1){
-				 break;
-			 }
+			month = scanner.nextInt();
+
+			System.out.println("첫번째 요일을 입력하세요. (SU, MO, TU, WE, TH, FR, SA)");
+			System.out.print(DAY_PROMPT);
+			day = scanner.next();
+
+			Calendar.CalendarShowing(year, month, day);
+
+			if (month == -1) {
+				break;
+			}
+		}
+
 	}
-			
-	
-	}				
-
-
 
 }
