45ab2fb71e1854add452669469f5b4cb2d07d324
トマト <@DESKTOP-E0F80M6>
2018-04-16 18:38:01 +0900

    on the way updating

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -1,33 +1,54 @@
 package honux.calendar;
 
-import java.util.Scanner;
-
 public class Calendar {
 
-public static void CalendarShowing(int year, int month) {
-	int[] daysOfEachMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-	Scanner scanner = 
-		// 月を入力してください。
+	public static void CalendarShowing(int year, int month, int remainder) {
+		int[] datesOfEachMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+		int[] datesOfLeapYear = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 
-		while(true) {
-			System.out.println("月を入力してください。");
-			System.out.print("cal> ");
-			
-			int number = scanner.nextInt();
-			if (number != -1) {
+		String space = "   ";
 
-		
-				System.out.printf("%d월은 %d일까지 있습니다. \n", number, daysOfEachMonth[number - 1]);
-				System.out.printf("        <%4d년%3d월 달력>        \n", number );
-				System.out.println("  일   월   화   수   목   금   토");
-				System.out.println("----------------------");
-				for (int i = 1; i <= daysOfEachMonth[number-1]; i++) {
-					System.out.printf("%3d/n",i);
+		System.out.printf("%d월은 %d일까지 있습니다. \n", month, datesOfEachMonth[month - 1]);
+		System.out.printf("   <%4d년%3d월 달력>   \n", year, month);
+		System.out.println("  일   월   화   수   목   금   토");
+		System.out.println("----------------------");
+
+		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+
+			for (int i = 0; i < remainder; i++) {
+				System.out.print(space);
+			}
+
+			for (int i = 1; i <= datesOfLeapYear[month - 1]; i++) {
+
+				System.out.printf("%3d", i);
+				if (remainder == 0) {
+					if (i % 7 == 0) {
+						System.out.println();
+					}
+				} else if (i % 7 == (remainder - 1)) {
+					System.out.println();
 				}
-				
-			} else {
+			}
+		} else {
+			for (int i = 0; i < remainder; i++) {
+				System.out.print(space);
+			}
 
+			for (int i = 1; i <= datesOfEachMonth[month - 1]; i++) {
+
+				System.out.printf("%3d", i);
+				if (remainder == 0) {
+					if (i % 7 == 0) {
+						System.out.println();
+					}
+				} else if (i % 7 == (remainder - 1)) {
+					System.out.println();
+				}
+			}
 		}
+
+		System.out.println();
+		System.out.println();
 	}
-}
 }
