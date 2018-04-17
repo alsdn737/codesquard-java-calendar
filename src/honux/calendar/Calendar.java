234157ed94f3812d8ac683a95366a0c4d72addb5
トマト <@DESKTOP-E0F80M6>
2018-04-17 12:32:14 +0900

    automatically completed calendar is done

--- src/honux/calendar/Calendar.java
+++ src/honux/calendar/Calendar.java
@@ -5,7 +5,10 @@ public class Calendar {
 	public static void CalendarShowing(int year, int month, int remainder) {
 		int[] datesOfEachMonth = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
 		int[] datesOfLeapYear = { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
-
+		int[] plusDays = { 3, 0, 3, 2, 3, 2, 3, 3, 2, 3, 2, 3 };
+		int[] plusDaysOfLeapYear = { 3, 1, 3, 2, 3, 2, 3, 3, 2, 3, 2, 3 };
+		int sumDays = 0;
+		
 		String space = "   ";
 
 		System.out.printf("%d월은 %d일까지 있습니다. \n", month, datesOfEachMonth[month - 1]);
@@ -15,22 +18,41 @@ public class Calendar {
 
 		if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
 
+			for (int i = 0; i < (month-1) ; i++) {
+				sumDays = sumDays + plusDaysOfLeapYear[i];
+			}
+			remainder = (remainder+sumDays)%7;
+			
 			for (int i = 0; i < remainder; i++) {
 				System.out.print(space);
 			}
 
+			
+			
 			for (int i = 1; i <= datesOfLeapYear[month - 1]; i++) {
 
 				System.out.printf("%3d", i);
 				if (remainder == 0) {
-					if (i % 7 == 0) {
+					if(i == datesOfLeapYear[month -1] && i%7 == 0) {
+						continue;
+					}
+				else if (i % 7 == 0 && i != datesOfLeapYear[month -1]) {
 						System.out.println();
 					}
-				} else if (i % 7 == (remainder - 1)) {
+				} else if (i % 7 == (7-remainder)&& i != datesOfEachMonth[month -1]) {
+					System.out.println();
 					System.out.println();
 				}
 			}
-		} else {
+		}
+		
+		else {
+			
+			for (int i = 0; i < (month-1) ; i++) {
+				sumDays = sumDays + plusDays[i];
+			}
+			remainder = (remainder+sumDays)%7;
+			
 			for (int i = 0; i < remainder; i++) {
 				System.out.print(space);
 			}
@@ -39,10 +61,13 @@ public class Calendar {
 
 				System.out.printf("%3d", i);
 				if (remainder == 0) {
-					if (i % 7 == 0) {
+					if(i == datesOfEachMonth[month -1] && i%7 == 0) {
+						break;
+					}
+				else if (i % 7 == 0 && i != datesOfEachMonth[month -1]) {
 						System.out.println();
 					}
-				} else if (i % 7 == (remainder - 1)) {
+				} else if (i % 7 == (7-remainder  )&& i != datesOfEachMonth[month -1]) {
 					System.out.println();
 				}
 			}
